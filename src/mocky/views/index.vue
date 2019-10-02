373717877857e09186e285a602dd75b638a3659f
oker <cool@oker.com>
2019-10-02 22:45:45 +0800

    [!] add ace editor

--- src/mocky/views/index.vue
+++ src/mocky/views/index.vue
@@ -8,6 +8,12 @@
         </el-button>
 
 
+        <div>
+            <editor v-model="content" @init="editorInit" lang="json" theme="tomorrow_night_blue"
+            width="500px" height="400px"></editor>
+        </div>
+
+
     </div>
     </div>
 </template>
@@ -16,10 +22,27 @@
     import * as mocky from "../api/mocky";
 
     export default {
+        name: 'aceEditor',
         data() {
-            return {};
+            return {
+                content: ''
+            };
+        },
+        components:{
+          editor: require('vue2-ace-editor')
         },
         methods: {
+            editorInit(){
+              require('brace/ext/language_tools')
+              require('brace/mode/yaml')
+              require('brace/mode/javascript')
+              require('brace/mode/json')
+              require('brace/mode/less')
+              require('brace/theme/tomorrow_night_blue')
+              require('brace/snippets/javascript')
+              require('brace/snippets/json')
+
+            },
             test() {
                 let req = {
                     statusCode: 200

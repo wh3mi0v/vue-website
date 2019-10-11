baf0e91118be1c2014a6e3f4425ed7ff7f67c5e5
oker <cool@oker.com>
2019-10-11 13:38:26 +0800

    [!] docs test website

--- src/mocky/views/index.vue
+++ src/mocky/views/index.vue
@@ -1,18 +1,53 @@
 <template>
     <div class="wrap">
         <h1>Mocky Http Server</h1>
-        <el-button
-                type="primary"
-                size="big"
-                icon="el-icon-plus" @click="test">添加
-        </el-button>
+
+        <div class="scrollbar blockLayout">
+            <el-form style="width: 50%"
+                     :model="ruleForm"
+                     status-icon
+                     ref="ruleForm"
+                     label-width="100px">
+
+                <el-form-item label="Status Code">
+                    <el-select v-model="ruleForm.password" placeholder="活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
 
 
-        <div>
-            <editor v-model="content" @init="editorInit" lang="json" theme="tomorrow_night_blue"
-            width="500px" height="400px"></editor>
+                <el-form-item label="Content Type">
+                    <el-col :span="11">
+                        <el-select v-model="ruleForm.password" placeholder="活动区域">
+                            <el-option label="区域一fefaefffffffffffffffffffffff" value="shanghai"></el-option>
+                            <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
+                    </el-col>
+                    <el-col class="line" :span="2"></el-col>
+                    <el-select v-model="ruleForm.password" placeholder="活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                    <el-col :span="11">
+                    </el-col>
+                </el-form-item>
+
+
+                <el-form-item label="Body">
+
+                    <div class="editor">
+                        <editor v-model="content" @init="editorInit" lang="json" theme="tomorrow"
+                                width="500px" height="200px" :options="editOptions" ></editor>
+                    </div>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="test">生成模拟请求</el-button>
+                    <el-button @click="test">高级设置</el-button>
+                </el-form-item>
+            </el-form>
         </div>
-
 
     </div>
     </div>
@@ -25,23 +60,51 @@
         name: 'aceEditor',
         data() {
             return {
-                content: ''
+                content: '',
+                ruleForm: {
+                    old_password: "",
+                    checkPass: "",
+                    password: ""
+                },
+                editOptions: {
+                    enableBasicAutocompletion: true,
+                    enableSnippets: true,
+                    enableLiveAutocompletion: false,
+                    readOnly: false,
+                    showInvisibles: true,
+                    showPrintMargin: false,
+                    showGutter: false,
+                    showHighlightActiveLine: false,
+                    selectionStyle: "text",
+                    cursorStyle: "wide",//"ace"|"slim"|"smooth"|"wide"
+                    behavioursEnabled: true,
+                    wrapBehavioursEnabled: true,
+                    autoScrollEditorIntoView: true,
+                    copyWithEmptySelection: true,
+                    navigateWithinSoftTabs: true
+                }
+
             };
         },
-        components:{
-          editor: require('vue2-ace-editor')
+        components: {
+            editor: require('vue2-ace-editor')
         },
         methods: {
-            editorInit(){
-              require('brace/ext/language_tools')
-              require('brace/mode/yaml')
-              require('brace/mode/javascript')
-              require('brace/mode/json')
-              require('brace/mode/less')
-              require('brace/theme/tomorrow_night_blue')
-              require('brace/snippets/javascript')
-              require('brace/snippets/json')
-
+            editorInit() {
+                require('brace/ext/language_tools')
+                require('brace/mode/yaml')
+                require('brace/mode/javascript')
+                require('brace/mode/json')
+                require('brace/mode/less')
+                require('brace/theme/tomorrow')
+                require('brace/snippets/javascript')
+                require('brace/snippets/json')
+                // var editor = ace.edit(id);
+                // editor.setTheme("ace/theme/tomorrow");
+                // editor.setHighlightActiveLine(false);
+                // editor.setShowPrintMargin(false);
+                // editor.renderer.setShowGutter(false);
+                // editor.getSession().setMode("ace/mode/json");
             },
             test() {
                 let req = {
@@ -61,5 +124,7 @@
 </script>
 
 <style scoped>
-
+    .editor {
+        border: 1px solid #DCDFE6;
+    }
 </style>
